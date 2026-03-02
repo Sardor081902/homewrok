2318e2d4de0f159fda5ae63ecb1da0c3a821224f
Sardor081902 <erkinovsardor72@gmail.com>
2026-03-02 13:44:49 +0500

    second commit

--- app.js
+++ app.js
@@ -2,44 +2,70 @@ const bt1 = document.querySelector('.bt1');
 const input1 = document.getElementById('input1');
 const bt1_del = document.getElementById('bt1_del');
 
-const bt2 = document.querySelector('.bt2');
-const input2 = document.getElementById('input2');
-const bt2_del = document.getElementById('bt2_del');
-
-const bt3 = document.querySelector('.bt3');
-const input3 = document.getElementById('input3');
-const bt3_del = document.getElementById('bt3_del');
-
-const bt4 = document.querySelector('.bt4');
-const input4 = document.getElementById('input4');
-const bt4_del = document.getElementById('bt4_del')
+const list = document.querySelector('.list')
+const add = document.querySelector('.add')
 
 
-function texts(it, i, c) {
-    if (!it || !i || !c) return;
+// const bt2 = document.querySelector('.bt2');
+// const input2 = document.getElementById('input2');
+// const bt2_del = document.getElementById('bt2_del');
 
-    it.addEventListener("click", () => {
-        it.classList.toggle("act");
+// const bt3 = document.querySelector('.bt3');
+// const input3 = document.getElementById('input3');
+// const bt3_del = document.getElementById('bt3_del');
 
-        if (it.classList.contains("act")) {
-            c.style.visibility = "visible";
-            i.style.visibility = "visible";
-            i.focus();
+// const bt4 = document.querySelector('.bt4');
+// const input4 = document.getElementById('input4');
+// const bt4_del = document.getElementById('bt4_del')
+
+
+function texts(bt, input, bt_del, add, list) {
+    if (!bt || !input || !bt_del || !add || !list) return;
+
+    bt.addEventListener("click", () => {
+        bt.classList.add("act");
+
+        if (bt.classList.contains("act")) {
+            bt_del.style.visibility = "visible";
+            input.style.visibility = "visible";
+            add.style.visibility = "visible";
+            input.focus();
         } else {
-            c.style.visibility = "hidden";
-            i.style.visibility = "hidden";
+            bt_del.style.visibility = "hidden";
+            input.style.visibility = "hidden";
+            add.style.visibility = "hidden";
         }
     });
 
-    c.addEventListener("click", () => {
-        i.value = ""; 
-        i.focus();    
+    bt_del.addEventListener("click", () => {
+        list.innerHTML = ` `;
+        input.focus();
+    });
+
+    add.addEventListener("click", () => {
+
+        let li = document.createElement("li");
+        if (li.textContent.trim() !== ""){
+            li.textContent = input.value;
+            li.id = li.textContent
+        }else{
+            
+        }
+
+
+        li.addEventListener("click", () => {
+            li.remove()
+        })
+
+        list.appendChild(li);
+
+        input.value = "";
+        bt.click();
     });
 }
 
 
-texts(bt1, input1, bt1_del);
-texts(bt2, input2, bt2_del);
-texts(bt3, input3, bt3_del);
-texts(bt4, input4, bt4_del);
-~
+texts(bt1, input1, bt1_del, add, list);
+// texts(bt2, input2, bt2_del);
+// texts(bt3, input3, bt3_del);
+// texts(bt4, input4, bt4_del);
